2946553b3c4792a5dc91fbb3631ee8c5b305e1b6
Idoo0oo <dittosiimutt19@gmail.com>
2025-06-23 13:43:04 +0700

    menambahkan tailwind

--- gadingpro-admin-panel/vite.config.ts
+++ gadingpro-admin-panel/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3001, // Pastikan port ini sesuai dengan yang Anda gunakan
     host: true, // Ini akan membuat Vite mendengarkan di semua alamat IP yang tersedia
