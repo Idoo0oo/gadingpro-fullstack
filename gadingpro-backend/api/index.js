bb715151818d963d15d8967ed568cc2a0c697efe
sipaell <raffaeladitya1354@gmail.com>
2025-06-24 10:30:59 +0700

    fixing code

--- gadingpro-backend/api/index.js
+++ gadingpro-backend/api/index.js
@@ -1,9 +1,5 @@
-import app from '../server'
-
 // gadingpro-backend/api/index.js
 // Ini adalah entry point yang akan diakses oleh Vercel
 const app = require('../server'); // Mengimpor aplikasi Express dari server.js
 
-module.exports = app;
-
-export default app;
+module.exports = app; // Menggunakan CommonJS export
